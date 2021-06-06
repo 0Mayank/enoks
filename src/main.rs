9329b6100f65ab4c0a4392c74c8084c9246081da
0Mayank <1mayanktomar@gmail.com>
2021-06-06 16:41:47 +0530

    added Game, Snake and Apple structs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+struct Snake {
+}
+
+struct Apple {
+
+}
+
+struct Game {
+    height: u8,
+    width: u8,
+}
+
+impl Game {
+    fn new(height: u8, width: u8) -> Game{
+        Game {
+            height,
+            width,
+        }
+    }
+
+    fn render(&self) {
+        println!("Height: {}", self.height);
+        println!("Width: {}", self.width);
+    }
+}
+
+fn main() {
+    let game = Game::new(2,4);
+    game.render();
+}

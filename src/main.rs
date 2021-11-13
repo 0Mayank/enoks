ab3725373358f55e91edcc71763722b4cd3e198d
0Mayank <1mayanktomar@gmail.com>
2021-11-13 12:40:36 +0530

    Made a snake head

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use bevy::prelude::*;
 
+struct SnakeHead;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
+}
+
 fn main() {
     App::build()
         .add_startup_system(setup.system())
         .add_plugins(DefaultPlugins)
+        .add_startup_stage("game setup", SystemStage::single(spawn_snake.system()))
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
+
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead);
 }
